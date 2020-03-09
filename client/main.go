702a759ef18bd165b4d0f374e0a22753b53381d8
Beaver <b3-v3r@protonmail.com>
2020-03-09 12:54:33 +0200

    Adding hour holder

--- client/main.go
+++ client/main.go
@@ -1,45 +1,66 @@
-package main 
+package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"io"
-	"os"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"syscall"
 )
 
-func ReadFile( filename string ) (string, bool) {
-	file, err := os.Open( filename)
+func ReadFile(filename string) (string, bool) {
+	file, err := os.Open(filename)
 
 	if err != nil {
 		return " ", true
 	}
-	
-	text, err := ioutil.ReadAll( file )
-	
+
+	text, err := ioutil.ReadAll(file)
+
 	return string(text), false
 }
 
-
-func data_handler( w http.ResponseWriter, r *http.Request ) {
+func data_handler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Getting data json ")
-	file_text, err := ReadFile( "../stats/current_stat.json")
+	file_text, err := ReadFile("../stats/current_stat.json")
 
 	if err {
 		w.WriteHeader(404)
 		return
 	}
 
+	val, _ := strconv.Atoi(os.Args[1])
+	syscall.Kill(val, syscall.SIGINT)
+
 	w.WriteHeader(200)
-	io.WriteString( w, file_text)
+	io.WriteString(w, file_text)
+
 }
 
 func main() {
-	http.HandleFunc( "/get_json", data_handler )
 
+	if len(os.Args) != 2 {
+		fmt.Println("Usage example: go run main.go [pid pws deamon]")
+		os.Exit(0)
+	}
+
+	http.HandleFunc("/get_json", data_handler)
+
+	cdir, _ := os.Getwd()
+	test := fmt.Sprintf("%s%s", cdir, "/../client/")
+
+	err := os.Chdir(test)
+
+	if err != nil {
+		fmt.Println("test")
+	}
+
+	fmt.Println(os.Getwd())
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
-	log.Fatal( http.ListenAndServe(":2020", nil))
+	log.Fatal(http.ListenAndServe(":2020", nil))
 }
